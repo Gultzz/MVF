099bba3a7b4d92eab828bf26b81c548de2e16bfb
Gultzz <gustavo.schulr@gmail.com>
2023-07-26 22:32:23 -0300

    Adicionado nova pergunta

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -1,89 +1,124 @@
 const topics = [
   {
-    title: 'Alcance resultados mais rapidamente',
-    description: "Um e-book sobre como ter foco é um guia prático e direto ao ponto, projetado para ajudar você a alcançar resultados de forma mais rápida e eficiente. Ele oferece estratégias testadas e comprovadas para melhorar sua concentração e produtividade."
+    title: "Alcance resultados mais rapidamente",
+    description:
+      "Um e-book sobre como ter foco é um guia prático e direto ao ponto, projetado para ajudar você a alcançar resultados de forma mais rápida e eficiente. Ele oferece estratégias testadas e comprovadas para melhorar sua concentração e produtividade.",
   },
   {
-    title: 'Supere a procrastinação',
-    description: "A procrastinação é um hábito difícil de quebrar, mas o e-book irá fornecer técnicas e abordagens eficazes para superar esse obstáculo e adotar uma mentalidade de ação consistente."
+    title: "Supere a procrastinação",
+    description:
+      "A procrastinação é um hábito difícil de quebrar, mas o e-book irá fornecer técnicas e abordagens eficazes para superar esse obstáculo e adotar uma mentalidade de ação consistente.",
   },
   {
-    title: 'Desenvolva habilidades de autodisciplina',
-    description: 'A melhoria do foco requer autodisciplina, e o e-book irá orientá-lo a desenvolver essa habilidade fundamental para alcançar seus objetivos.'
+    title: "Desenvolva habilidades de autodisciplina",
+    description:
+      "A melhoria do foco requer autodisciplina, e o e-book irá orientá-lo a desenvolver essa habilidade fundamental para alcançar seus objetivos.",
   },
   {
-    title: 'Aumente sua produtividade',
-    description: 'Através de estratégias específicas e exercícios práticos, o e-book irá ajudá-lo a maximizar sua produtividade, permitindo que você faça mais em menos tempo.'
+    title: "Aumente sua produtividade",
+    description:
+      "Através de estratégias específicas e exercícios práticos, o e-book irá ajudá-lo a maximizar sua produtividade, permitindo que você faça mais em menos tempo.",
   },
   {
-    title: 'Desbloqueie seu potencial máximo',
-    description: 'Ao dominar o foco, você desbloqueará seu potencial máximo. Você será capaz de se concentrar nas metas e objetivos importantes e alcançar resultados extraordinários em sua vida pessoal e profissional.'
+    title: "Desbloqueie seu potencial máximo",
+    description:
+      "Ao dominar o foco, você desbloqueará seu potencial máximo. Você será capaz de se concentrar nas metas e objetivos importantes e alcançar resultados extraordinários em sua vida pessoal e profissional.",
   },
   {
-    title: 'Aumente sua confiança',
-    description: 'À medida que você desenvolve e aprimora suas habilidades de foco, sua confiança também aumentará. Você se sentirá mais capacitado para enfrentar desafios e superar obstáculos em seu caminho.'
-  }
-]
+    title: "Aumente sua confiança",
+    description:
+      "À medida que você desenvolve e aprimora suas habilidades de foco, sua confiança também aumentará. Você se sentirá mais capacitado para enfrentar desafios e superar obstáculos em seu caminho.",
+  },
+];
 
 const asks = [
   {
-    "title": "O que é um e-book de melhoria de foco?",
-    "description": "Um e-book de melhoria de foco é um livro digital que oferece orientações, estratégias e técnicas para ajudar as pessoas a desenvolverem e aprimorarem sua capacidade de concentração e foco."
+    title: "O que é um e-book de melhoria de foco?",
+    description:
+      "Um e-book de melhoria de foco é um livro digital que oferece orientações, estratégias e técnicas para ajudar as pessoas a desenvolverem e aprimorarem sua capacidade de concentração e foco.",
   },
   {
-    title: 'Como posso ter certeza de que esse e-book será eficaz para mim?',
-    description: 'O e-book foi desenvolvido com base em princípios comprovados e técnicas de melhoria de foco que são aplicáveis a uma ampla gama de pessoas. No entanto, cada indivíduo é único, e os resultados podem variar. O e-book oferece uma abordagem abrangente, incluindo várias estratégias e exercícios que podem ser adaptados às suas necessidades e preferências. A chave para obter benefícios significativos é implementar as técnicas propostas de forma consistente e ajustá-las conforme necessário para se adequar ao seu estilo de vida. O e-book fornecerá as ferramentas e conhecimentos necessários para melhorar seu foco, mas a aplicação e a dedicação são essenciais para alcançar resultados tangíveis.',
+    title: "Como posso ter certeza de que esse e-book será eficaz para mim?",
+    description:
+      "O e-book foi desenvolvido com base em princípios comprovados e técnicas de melhoria de foco que são aplicáveis a uma ampla gama de pessoas. No entanto, cada indivíduo é único, e os resultados podem variar. O e-book oferece uma abordagem abrangente, incluindo várias estratégias e exercícios que podem ser adaptados às suas necessidades e preferências. A chave para obter benefícios significativos é implementar as técnicas propostas de forma consistente e ajustá-las conforme necessário para se adequar ao seu estilo de vida. O e-book fornecerá as ferramentas e conhecimentos necessários para melhorar seu foco, mas a aplicação e a dedicação são essenciais para alcançar resultados tangíveis.",
   },
   {
-    title: "Você já se perguntou quantas oportunidades e conquistas você está perdendo devido à falta de foco? E se você pudesse desbloquear seu potencial máximo e aproveitar ao máximo cada momento?",
-    description: 'Nosso e-book sobre melhoria de foco foi criado exatamente para ajudar você a superar as limitações da falta de concentração. Imagine como seria sua vida se você pudesse se concentrar profundamente nas tarefas mais importantes, alcançando níveis de produtividade que você nunca imaginou possíveis. Ao investir neste e-book, você estará adquirindo um guia completo, repleto de estratégias comprovadas, dicas práticas e exercícios eficazes. Comece a transformar sua vida agora mesmo, aproveitando todas as oportunidades e alcançando seus objetivos com clareza, disciplina e foco inabalável.',
+    title:
+      "Você já se perguntou quantas oportunidades e conquistas você está perdendo devido à falta de foco? E se você pudesse desbloquear seu potencial máximo e aproveitar ao máximo cada momento?",
+    description:
+      "Nosso e-book sobre melhoria de foco foi criado exatamente para ajudar você a superar as limitações da falta de concentração. Imagine como seria sua vida se você pudesse se concentrar profundamente nas tarefas mais importantes, alcançando níveis de produtividade que você nunca imaginou possíveis. Ao investir neste e-book, você estará adquirindo um guia completo, repleto de estratégias comprovadas, dicas práticas e exercícios eficazes. Comece a transformar sua vida agora mesmo, aproveitando todas as oportunidades e alcançando seus objetivos com clareza, disciplina e foco inabalável.",
   },
   {
-    title: 'Já tentei várias abordagens para melhorar meu foco, mas até agora nada funcionou. Como esse e-book pode fazer a diferença?',
-    description: "É compreensível que você possa ter tentado várias abordagens sem sucesso. O e-book oferece uma perspectiva abrangente, combinando diferentes técnicas e estratégias de melhoria de foco. Ele pode fornecer insights sobre os desafios comuns que as pessoas enfrentam ao tentar se concentrar e oferecer soluções específicas para superá-los. Além disso, o e-book pode ajudá-lo a identificar possíveis obstáculos pessoais e fornecer orientações para superá-los. É importante abordar a melhoria do foco de maneira holística, combinando várias técnicas e ajustando-as para atender às suas necessidades individuais. O e-book pode oferecer novas abordagens e estratégias que podem ser o diferencial para você finalmente alcançar a melhoria desejada."
+    title:
+      "Já tentei várias abordagens para melhorar meu foco, mas até agora nada funcionou. Como esse e-book pode fazer a diferença?",
+    description:
+      "É compreensível que você possa ter tentado várias abordagens sem sucesso. O e-book oferece uma perspectiva abrangente, combinando diferentes técnicas e estratégias de melhoria de foco. Ele pode fornecer insights sobre os desafios comuns que as pessoas enfrentam ao tentar se concentrar e oferecer soluções específicas para superá-los. Além disso, o e-book pode ajudá-lo a identificar possíveis obstáculos pessoais e fornecer orientações para superá-los. É importante abordar a melhoria do foco de maneira holística, combinando várias técnicas e ajustando-as para atender às suas necessidades individuais. O e-book pode oferecer novas abordagens e estratégias que podem ser o diferencial para você finalmente alcançar a melhoria desejada.",
   },
   {
-    title: "Estou preocupado se as técnicas de melhoria de foco deste e-book são cientificamente comprovadas. Existe embasamento científico por trás das estratégias propostas?",
-    description: "Sim, é importante garantir que as estratégias propostas sejam baseadas em evidências científicas. O e-book pode fornecer referências ou citar estudos científicos relevantes para respaldar as técnicas propostas. Você também pode realizar sua própria pesquisa adicional para verificar a validade e a eficácia das estratégias mencionadas. Além disso, o e-book pode ser desenvolvido por especialistas na área, como psicólogos, coaches ou profissionais experientes em melhoria de foco, que possuem conhecimento prático e embasamento científico para respaldar as técnicas propostas."
+    title:
+      "Estou preocupado se as técnicas de melhoria de foco deste e-book são cientificamente comprovadas. Existe embasamento científico por trás das estratégias propostas?",
+    description:
+      "Sim, é importante garantir que as estratégias propostas sejam baseadas em evidências científicas. O e-book pode fornecer referências ou citar estudos científicos relevantes para respaldar as técnicas propostas. Você também pode realizar sua própria pesquisa adicional para verificar a validade e a eficácia das estratégias mencionadas. Além disso, o e-book pode ser desenvolvido por especialistas na área, como psicólogos, coaches ou profissionais experientes em melhoria de foco, que possuem conhecimento prático e embasamento científico para respaldar as técnicas propostas.",
   },
 
-    {
-      "title": "Quais são os benefícios de ler um e-book sobre como melhorar o foco?",
-      "description": "Os benefícios de ler um e-book sobre melhoria de foco incluem a capacidade de aumentar a produtividade, melhorar a concentração, lidar com distrações, aprimorar a memória e alcançar metas com mais eficácia."
-    },
-    {
-      "title": "Como posso adquirir um e-book sobre melhoria de foco?",
-      "description": "Normalmente, os e-books estão disponíveis para compra em lojas online, como Amazon, Google Play Books, Kobo e outras plataformas de venda de livros digitais. Basta pesquisar o título específico ou navegar nas categorias relevantes para encontrar e adquirir o e-book desejado."
-    },
-    {
-      "title": "É possível melhorar o foco por meio de um e-book? Como isso funciona?",
-      "description": "Sim, é possível melhorar o foco por meio de um e-book. Os e-books oferecem orientações, estratégias e técnicas específicas para desenvolver a capacidade de concentração e foco. Ao aplicar essas técnicas de forma consistente e seguir as recomendações fornecidas, é possível melhorar gradativamente a habilidade de concentração."
-    },
-    {
-      "title": "Existem exercícios práticos incluídos no e-book para aprimorar o foco?",
-      "description": "Sim, muitos e-books de melhoria de foco incluem exercícios práticos. Esses exercícios podem incluir práticas de meditação, técnicas de respiração, exercícios de atenção plena, atividades de concentração e outros métodos para fortalecer a capacidade de foco."
-    },
-    {
-      "title": "Quanto tempo leva para ver os resultados ao seguir as técnicas descritas no e-book?",
-      "description": "O tempo para ver resultados pode variar de pessoa para pessoa, dependendo do nível de comprometimento e prática. Alguns podem experimentar melhorias imediatas, enquanto outros podem precisar de um tempo maior para consolidar as técnicas e colher os benefícios a longo prazo. A consistência e a aplicação diligente das técnicas descritas no e-book são fundamentais para obter resultados satisfatórios."
-    },
-    {
-      "title": "Posso ler o e-book em qualquer dispositivo ou é necessário um formato específico?",
-      "description": "Geralmente, os e-books podem ser lidos em uma variedade de dispositivos, incluindo smartphones, tablets, leitores de e-books dedicados e computadores. Os formatos mais comuns para e-books incluem PDF, ePub e MOBI. Nossos e-books são todos no formato PDF, verifique se o dispositivo que você possui é compatível com o formato do e-book antes de adquiri-lo."
-    },
-    {
-      "title": "O e-book contém dicas específicas para melhorar o foco no trabalho ou nos estudos?",
-      "description": "Sim, nosso e-book de melhoria de foco contêm dicas específicas para melhorar o foco no trabalho ou nos estudos. Essas dicas podem abordar questões como gerenciamento do tempo, organização do espaço de trabalho, técnicas de estudo eficazes, estratégias para evitar distrações e muito mais."
-    },
-    {
-      "title": "O e-book fornece estratégias para lidar com distrações e manter o foco por períodos prolongados?",
-      "description": "Sim, nosso e-book de melhoria de foco fornece estratégias para lidar com distrações e manter o foco por períodos prolongados. Essas estratégias podem incluir técnicas de gerenciamento de distrações, práticas de atenção plena, definição de metas claras, intervalos regulares de descanso e outras abordagens eficazes para melhorar a concentração."
-    },
-    {
-      "title": "É possível obter um reembolso se o e-book não atender às minhas expectativas?",
-      "description": "Sim, caso você não tenha encontrado resultados com o e-book você terá 7 dias para solicitar o reembolso."
-    }
+  {
+    title:
+      "Quais são os benefícios de ler um e-book sobre como melhorar o foco?",
+    description:
+      "Os benefícios de ler um e-book sobre melhoria de foco incluem a capacidade de aumentar a produtividade, melhorar a concentração, lidar com distrações, aprimorar a memória e alcançar metas com mais eficácia.",
+  },
+  {
+    title: "Como posso adquirir um e-book sobre melhoria de foco?",
+    description:
+      "Normalmente, os e-books estão disponíveis para compra em lojas online, como Amazon, Google Play Books, Kobo e outras plataformas de venda de livros digitais. Basta pesquisar o título específico ou navegar nas categorias relevantes para encontrar e adquirir o e-book desejado.",
+  },
+  {
+    title:
+      "É possível melhorar o foco por meio de um e-book? Como isso funciona?",
+    description:
+      "Sim, é possível melhorar o foco por meio de um e-book. Os e-books oferecem orientações, estratégias e técnicas específicas para desenvolver a capacidade de concentração e foco. Ao aplicar essas técnicas de forma consistente e seguir as recomendações fornecidas, é possível melhorar gradativamente a habilidade de concentração.",
+  },
+  {
+    title:
+      "Existem exercícios práticos incluídos no e-book para aprimorar o foco?",
+    description:
+      "Sim, muitos e-books de melhoria de foco incluem exercícios práticos. Esses exercícios podem incluir práticas de meditação, técnicas de respiração, exercícios de atenção plena, atividades de concentração e outros métodos para fortalecer a capacidade de foco.",
+  },
+  {
+    title:
+      "Quanto tempo leva para ver os resultados ao seguir as técnicas descritas no e-book?",
+    description:
+      "O tempo para ver resultados pode variar de pessoa para pessoa, dependendo do nível de comprometimento e prática. Alguns podem experimentar melhorias imediatas, enquanto outros podem precisar de um tempo maior para consolidar as técnicas e colher os benefícios a longo prazo. A consistência e a aplicação diligente das técnicas descritas no e-book são fundamentais para obter resultados satisfatórios.",
+  },
+  {
+    title:
+      "Posso ler o e-book em qualquer dispositivo ou é necessário um formato específico?",
+    description:
+      "Geralmente, os e-books podem ser lidos em uma variedade de dispositivos, incluindo smartphones, tablets, leitores de e-books dedicados e computadores. Os formatos mais comuns para e-books incluem PDF, ePub e MOBI. Nossos e-books são todos no formato PDF, verifique se o dispositivo que você possui é compatível com o formato do e-book antes de adquiri-lo.",
+  },
+  {
+    title:
+      "O e-book contém dicas específicas para melhorar o foco no trabalho ou nos estudos?",
+    description:
+      "Sim, nosso e-book de melhoria de foco contêm dicas específicas para melhorar o foco no trabalho ou nos estudos. Essas dicas podem abordar questões como gerenciamento do tempo, organização do espaço de trabalho, técnicas de estudo eficazes, estratégias para evitar distrações e muito mais.",
+  },
+  {
+    title:
+      "O e-book fornece estratégias para lidar com distrações e manter o foco por períodos prolongados?",
+    description:
+      "Sim, nosso e-book de melhoria de foco fornece estratégias para lidar com distrações e manter o foco por períodos prolongados. Essas estratégias podem incluir técnicas de gerenciamento de distrações, práticas de atenção plena, definição de metas claras, intervalos regulares de descanso e outras abordagens eficazes para melhorar a concentração.",
+  },
+  {
+    title:
+      "É possível obter um reembolso se o e-book não atender às minhas expectativas?",
+    description:
+      "Sim, caso você não tenha encontrado resultados com o e-book você terá 7 dias para solicitar o reembolso.",
+  },
+  {
+    title: "Como eu vou receber meu e-book?",
+    description:
+      "Em aproximadamente 30 minutos após a confirmação da compra, ele estará em seu email.",
+  },
+];
 
-]
-
-export {topics, asks}
+export {topics, asks};

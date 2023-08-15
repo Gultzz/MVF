82c15e5a6e6c46511fc3975034eacf973f01690c
Gultzz <gustavo.schulr@gmail.com>
2023-08-15 07:26:17 -0300

    Build Index

--- src/utils/colors.ts
+++
@@ -1,9 +0,0 @@
-export const colors = {
-  yellow: '#FFEB34',
-  black: '#131212',
-  gray: '#696969',
-  lightGray: '#BFBFBF',
-  mediumGray: '#999999',
-  green: '#0FCD4F',
-  purple: '#6800CF'
-}
